749572cde66d5c40d386480761b550e6cf0a4de3
Hakuho <hakuho01@gmail.com>
2021-12-22 03:01:29 +0900

    prefix変更

--- bot.rb
+++ bot.rb
@@ -5,11 +5,12 @@ Dotenv.load
 TOKEN = ENV['TOKEN']
 CLIENT_ID = ENV['CLIENT_ID']
 
-bot = Discordrb::Commands::CommandBot.new token: TOKEN, client_id: CLIENT_ID, prefix: '!ae'
+bot = Discordrb::Commands::CommandBot.new token: TOKEN, client_id: CLIENT_ID, prefix: '!ae '
 
 # 何かメッセージが入力されたら実行
 bot.message(contains: /^(?!http)[!-~]{20,}/) do |event|
   event.respond 'ハッシュ値やアクセストークンの疑いがある文字列を検知しました。'
+  event.add_role
 end
 
 bot.run
