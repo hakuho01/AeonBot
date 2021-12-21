9a10077264f8e1966dae7274aea8c2072ca58a0a
Hakuho <hakuho01@gmail.com>
2021-12-22 02:24:11 +0900

    正規表現でアクセストークンやハッシュ値の疑いを検知してアラートを返す

--- bot.rb
+++ bot.rb
@@ -8,8 +8,8 @@ CLIENT_ID = ENV['CLIENT_ID']
 bot = Discordrb::Commands::CommandBot.new token: TOKEN, client_id: CLIENT_ID, prefix: '!ae'
 
 # 何かメッセージが入力されたら実行
-bot.message do |event|
-  event.respond 'Hello, world!'
+bot.message(contains: /^(?!http)[!-~]{20,}/) do |event|
+  event.respond 'ハッシュ値やアクセストークンの疑いがある文字列を検知しました。'
 end
 
 bot.run
