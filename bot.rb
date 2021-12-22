6e9fc652ffa040197bd728c0ca7976400c4ea61d
Hakuho <hakuho01@gmail.com>
2021-12-22 23:21:46 +0900

    APIを叩くクラスを追加

--- bot.rb
+++ bot.rb
@@ -4,13 +4,15 @@ require 'dotenv'
 Dotenv.load
 TOKEN = ENV['TOKEN']
 CLIENT_ID = ENV['CLIENT_ID']
+SERVER_ID = ENV['SERVER_ID']
+ROLE_ID = ENV['ROLE_ID']
 
 bot = Discordrb::Commands::CommandBot.new token: TOKEN, client_id: CLIENT_ID, prefix: '!ae '
+api = Discordrb::API::Server
 
-# 何かメッセージが入力されたら実行
 bot.message(contains: /^(?!http)[!-~]{20,}/) do |event|
   event.respond 'ハッシュ値やアクセストークンの疑いがある文字列を検知しました。'
-  event.add_role
+  api.add_member_role(TOKEN, SERVER_ID, event.user.id, ROLE_ID)
 end
 
 bot.run
