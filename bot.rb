0ad0a8b74c9c9f2c47f3a1d18a28272fba28f4be
Hakuho <hakuho01@gmail.com>
2021-12-23 01:55:10 +0900

    メンションで生存確認機能を追加。チャンネルメンション等で誤検知しないように正規表現を変更

--- bot.rb
+++ bot.rb
@@ -10,9 +10,13 @@ ROLE_ID = ENV['ROLE_ID']
 bot = Discordrb::Commands::CommandBot.new token: TOKEN, client_id: CLIENT_ID, prefix: '!ae '
 api = Discordrb::API::Server
 
-bot.message(contains: /^(?!http)[!-~]{20,}/) do |event|
-  event.respond 'ハッシュ値やアクセストークンの疑いがある文字列を検知しました。'
-  api.add_member_role("Bot #{TOKEN}", SERVER_ID, event.user.id, ROLE_ID)
+bot.mention do |event|
+    event.respond '生きてまーす。'
+end
+
+bot.message(contains: /^(?!http)(?!.*<@)(?!.*<#)[!-~]{20,}$/) do |event|
+    event.respond 'ハッシュ値やアクセストークンの疑いがある文字列を検知しました。'
+    api.add_member_role("Bot #{TOKEN}", SERVER_ID, event.user.id, ROLE_ID)
 end
 
 bot.run
