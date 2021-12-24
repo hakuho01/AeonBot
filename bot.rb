5305869768d5e5ca617756e58c78caca1f717947
Hakuho <hakuho01@gmail.com>
2021-12-25 03:44:33 +0900

    共同編集者が増えたのでコメントを追加

--- bot.rb
+++ bot.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
+# gem読み込み
 require 'discordrb'
 require 'dotenv'
 require 'json'
 
+# 環境変数読み込み
 Dotenv.load
 TOKEN = ENV['TOKEN']
 CLIENT_ID = ENV['CLIENT_ID']
@@ -14,10 +16,12 @@ DEPRIVATE_ROLE_ID = ENV['DEPRIVATE_ROLE_ID']
 bot = Discordrb::Commands::CommandBot.new token: TOKEN, client_id: CLIENT_ID, prefix: '!ae '
 api = Discordrb::API::Server
 
+# メンション時の反応
 bot.mention do |event|
   event.respond '馴れ馴れしくするな……'
 end
 
+# ハッシュ検知時の反応
 bot.message(contains: /^(?!http)(?!.*<@)(?!.*<#)(?!.*<:)(?!.*<a:)(?!.*<t:)[!-~]{19,}$/) do |event|
   event.respond 'ハッシュ値やアクセストークンの疑いがある文字列を検知した。'
   member_info = api.resolve_member("Bot #{TOKEN}", SERVER_ID, event.user.id)
@@ -31,4 +35,5 @@ bot.message(contains: /^(?!http)(?!.*<@)(?!.*<#)(?!.*<:)(?!.*<a:)(?!.*<t:)[!-~]{
   end
 end
 
+# bot起動
 bot.run
