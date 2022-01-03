4baeb01c024e50d880eb031c755506c2860ce454
Hakuho <hakuho01@gmail.com>
2022-01-03 21:23:47 +0900

    Rubocopによる文法訂正(固定文字列のfreeze)

--- config/constants.rb
+++ config/constants.rb
@@ -1,17 +1,19 @@
-module Constants
-    module Speech
-        RESPONSE_MENTION = [
-            '馴れ馴れしくするな……',
-            '寂しいのか？',
-            'zzz……ね、眠ってなどいない！',
-            '随分と暇そうだな',
-            'いつでもお前たちを見ているぞ',
-            '……物好きな奴だな'
-        ]
+# frozen_string_literal: true
 
-        # ハッシュ値検知関連
-        DETECT_HASH = 'ハッシュ値やアクセストークンの疑いがある文字列を検知した。'
-        PURGE = 'さらなる罪を重ねるか……。ならば、粛清する！'
-        PURGE_TEST_MODE = '……テストモードか。命拾いしたな'
-    end
+module Constants
+  module Speech
+    RESPONSE_MENTION = [
+      '馴れ馴れしくするな……',
+      '寂しいのか？',
+      'zzz……ね、眠ってなどいない！',
+      '随分と暇そうだな',
+      'いつでもお前たちを見ているぞ',
+      '……物好きな奴だな'
+    ].freeze
+
+    # ハッシュ値検知関連
+    DETECT_HASH = 'ハッシュ値やアクセストークンの疑いがある文字列を検知した。'
+    PURGE = 'さらなる罪を重ねるか……。ならば、粛清する！'
+    PURGE_TEST_MODE = '……テストモードか。命拾いしたな'
+  end
 end
