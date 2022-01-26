8386fa6c12968820fb3078ae6c15a8b576a03b24
Hakuho <hakuho01@gmail.com>
2022-01-27 00:28:56 +0900

    Revert "Revert "Merge pull request #35 from hakuho01/bugfix/20220125/fix_timezone""
    This reverts commit 770ea40e2f2816a130e1fcf8bf143588958cbf59.

--- util/time_util.rb
+++ util/time_util.rb
@@ -1,17 +1,21 @@
 require 'time'
 
 module TimeUtil
+  def now
+    return Time.now.localtime('+09:00')
+  end
+
   def parse_date_time(date_str, time_str)
-    return Time.strptime(date_str + ' ' + time_str + '+9:00', '%Y/%m/%d %H:%M%z')
+    return Time.strptime(date_str + ' ' + time_str + '+09:00', '%Y/%m/%d %H:%M%z')
   end
 
   def parse_min_time(time_str)
-    return Time.strptime(time_str + '+9:00', '%Y%m%d%H%M%z')
+    return Time.strptime(time_str + '+09:00', '%Y%m%d%H%M%z')
   end
 
   def format_min_time(time)
     return time.strftime('%Y%m%d%H%M')
   end
 
-  module_function :parse_date_time, :parse_min_time, :format_min_time
+  module_function :now, :parse_date_time, :parse_min_time, :format_min_time
 end
