4e85c69e8fefc14ea1b25612e19356610d2120a0
HAKUHO <hakuho01@gmail.com>
2022-05-05 20:04:06 +0900

    issue #75 複数画像のembedはできないが一旦実装

--- service/api_service.rb
+++ service/api_service.rb
@@ -8,9 +8,10 @@ require 'open-uri'
 require 'nokogiri'
 require 'openssl'
 require 'cgi'
+require 'simple_twitter'
+require 'time'
 
 class ApiService < Component
-
   # 楽天
   def rakuten(event)
     parsed_response = ApiUtil.get(Constants::URLs::RAKUTEN_GENRE)
@@ -45,6 +46,7 @@ class ApiService < Component
     end
   end
 
+  # wisdom guild
   def wisdom_guild(event)
     cardname = event.message.to_s.slice(/{{.*?}}/)[2..-3]
     encoded_cardname = CGI.escape(cardname)
@@ -70,5 +72,42 @@ class ApiService < Component
   #      wgurl = 'http://wonder.wisdom-guild.net/api/card-price/v1/' << '?' << 'api_key=hakuho01&name=' << cardname << '&timestamp=' << unixtime.to_s << '&api_sig=' << api_sig
   #      puts wgurl
   #      ApiUtil::get(wgurl)
+  end
+
+  # TwitterNSFWサムネイル表示
+  def twitter_thumbnail(event)
+    # ツイート情報を取得する
+    content = event.message.content
+    twitter_url = content.match(/https:\/\/twitter.com\/([a-zA-Z0-9_]+)\/status\/([0-9]+)/)
+    twitter_id = twitter_url[2]
+    token = ENV['TWITTER_BEARER_TOKEN']
+    client = SimpleTwitter::Client.new(bearer_token: token)
+    response = client.get_raw(Constants::URLs::TWITTER + twitter_id + '?tweet.fields=created_at,attachments,possibly_sensitive,public_metrics&expansions=author_id,attachments.media_keys&user.fields=profile_image_url&media.fields=media_key,type,url')
+    parsed_response = JSON.parse(response)
+
+    return if parsed_response['data']['possibly_sensitive'] == false
+
+    likes = parsed_response['data']['public_metrics']['like_count']
+    rts = parsed_response['data']['public_metrics']['retweet_count']
+    footer_text = "#{likes} Favs, #{rts} RTs"
+    author_name = parsed_response['includes']['users'][0]['name']
+    author_icon = parsed_response['includes']['users'][0]['profile_image_url']
+    author_url = "https://twitter.com/#{parsed_response['includes']['users'][0]['username']}"
+    event.send_embed do |embed|
+      embed.description = parsed_response['data']['text']
+      embed.colour = 0x1DA1F2
+      embed.timestamp = Time.parse(parsed_response['data']['created_at'])
+      embed.footer = Discordrb::Webhooks::EmbedFooter.new(
+        text: footer_text
+      )
+      embed.author = Discordrb::Webhooks::EmbedAuthor.new(
+        name: author_name,
+        url: author_url,
+        icon_url: author_icon
+      )
+    end
+    parsed_response['includes']['media'].each do |n|
+      event.respond n['url']
+    end
   end
 end
