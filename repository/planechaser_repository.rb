93b3751cab638302136a9d4407df33d12f4359d6
kirk3110 <saito.kiriko@gmail.com>
2022-07-10 19:14:39 +0900

    bind変数として書けるみたいなので書き方を変更

--- repository/planechaser_repository.rb
+++ repository/planechaser_repository.rb
@@ -2,6 +2,6 @@ require './framework/repository'
 
 class PlaneChaserRepository < Repository
   def select_plane(plane_num)
-    @db["SELECT * FROM planes WHERE id = #{plane_num}"].first
+    @db['SELECT * FROM planes WHERE id = ?', plane_num].first
   end
 end
